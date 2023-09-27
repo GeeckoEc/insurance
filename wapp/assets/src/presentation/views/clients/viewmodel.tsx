2c562df4743b5c00f8ba9fe95f5d70b74fa49332
Fernando Avilés <fernando.aviles.covena@gmail.com>
2023-09-27 10:01:23 -0500

    Nuevos cambios en Nuevo CLiente.

--- wapp/assets/src/presentation/views/clients/viewmodel.tsx
+++
@@ -1,9 +0,0 @@
-import React,{useState} from "react";
-
-export const [valores, definirValores] = useState({
-    nombre:     "",
-});
-
-const onChange = (propiedad: string, valor: any) => {
-    definirValores({...valores, [propiedad]:valor})
-}

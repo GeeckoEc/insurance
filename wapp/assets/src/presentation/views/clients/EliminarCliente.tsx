838e216fc049eca0ddc6c880bcb2dc96b75fe4fc
Fernando Avilés IX <fernando.aviles.covena@gmail.com>
2023-09-28 18:02:55 -0500

    Inicio Aliminar cliente

---
+++ wapp/assets/src/presentation/views/clients/EliminarCliente.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../../../../App";
